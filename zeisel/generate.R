688225065c4f7db4b6a43970fab15754296abf00
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-01-16 02:45:57 -0800

    Create a subsetted version of each dataset, for faster testing.

--- zeisel/generate.R
+++ zeisel/generate.R
@@ -1,17 +1,32 @@
 library(scRNAseq)
 sce <- ZeiselBrainData()
-
-library(HDF5Array)
-writeHDF5Array(assay(sce), filepath="zeisel.dense.h5", name="matrix", chunkdim=c(1000, 500))
-
-# Saving in version 3 of the 10x format.
-writeTENxMatrix(assay(sce), filepath="zeisel.tenx.h5", group="matrix")
-rhdf5::h5createGroup("zeisel.tenx.h5", "matrix/features")
-rhdf5::h5write(rownames(sce), "zeisel.tenx.h5", "matrix/features/id")
-rhdf5::h5write(rownames(sce), "zeisel.tenx.h5", "matrix/features/name")
-
+sub <- sce[1:1000,]
 library(zellkonverter)
-assay(sce) <- as(assay(sce), "dgCMatrix")
-writeH5AD(sce, file="zeisel.csc.h5ad")
+library(HDF5Array)
 
+for (s in c(FALSE, TRUE)) {
+    if (s) {
+        x <- sub
+        suffix <- "sub."
+    } else {
+        x <- sce
+        suffix <- ""
+    }
 
+    name <- sprintf("zeisel.dense.%sh5", suffix)
+    unlink(name)
+    writeHDF5Array(assay(x), filepath=name, name="matrix", chunkdim=c(1000, 500))
+
+    # Saving in version 3 of the 10x format.
+    name <- sprintf("zeisel.tenx.%sh5", suffix)
+    unlink(name)
+    writeTENxMatrix(assay(x), filepath=name, group="matrix")
+    rhdf5::h5createGroup(name, "matrix/features")
+    rhdf5::h5write(rownames(x), name, "matrix/features/id")
+    rhdf5::h5write(rownames(x), name, "matrix/features/name")
+
+    assay(x) <- as(assay(x), "dgCMatrix")
+    name <- sprintf("zeisel.csc.%sh5ad", suffix)
+    unlink(name)
+    writeH5AD(x, file=name)
+}
