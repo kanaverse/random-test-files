e5475269308bf33b1e06a41c352b9e41c76416a7
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-01-24 23:20:11 -0800

    Added scripts for the Bacher dataset.

---
+++ bacher-tcell/README.md
@@ -0,0 +1,4 @@
+# Bacher T cell single-cell RNA-seq dataset
+
+This takes the T cell dataset from [Bacher _et al._ (2020)](https://pubmed.ncbi.nlm.nih.gov/33296686/) and provides it in the 10X HDF5 and MatrixMarket formats.
+Run `generate.R` to construct the files.
---
+++ bacher-tcell/generate.R
@@ -0,0 +1,12 @@
+library(scRNAseq)
+sce <- BacherTCellData()
+
+# Saving in version 3 of the 10x format.
+library(DropletUtils)
+write10xCounts("mtx", assay(sce), version="3", overwrite=TRUE)
+
+# Saving in version 3 of the 10x format.
+source("../_scripts/compact10x.R")
+name <- "tenx.h5"
+unlink(name)
+compact10x(name, assay(sce), rownames(sce), rownames(sce))
