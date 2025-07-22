5825e7eeff54fb14d225312ad8ccb11708d647df
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-07-22 11:43:35 -0700

    Generate some alabaster-formatted files for testing.

---
+++ alabaster/generate.R
@@ -0,0 +1,40 @@
+library(scRNAseq)
+library(alabaster.sce)
+sce <- ZeiselBrainData()
+
+dump <- function(sce, path, ...) {
+    unlink(path, recursive=TRUE)
+    saveObject(sce, path, ...)
+    tfile <- paste0(path, ".tar.gz")
+    unlink(tfile)
+    tar(tfile, files=list.files(path, full=TRUE, recursive=TRUE), compression="gzip")
+}
+
+local({
+    assay(sce) <- as(assay(sce), "dgCMatrix")
+    dump(sce, "zeisel-brain-sparse")
+
+    mainExpName(sce) <- NULL 
+    altExps(sce) <- list()
+    dump(sce, "zeisel-brain-stripped")
+})
+
+local({
+    assay(sce) <- as.matrix(assay(sce))
+    dump(sce, "zeisel-brain-dense")
+})
+
+local({
+    library(scrapper)
+    res <- analyze(as(counts(sce), "dgCMatrix"), rna.subsets=list(Mito=grepl("^mt-", rownames(sce))))
+    sce2 <- convertAnalyzeResults(res)
+    dump(sce2, "zeisel-brain-sparse-results")
+    dump(sce2, "zeisel-brain-sparse-results-delayed", DelayedArray.preserve.ops=TRUE)
+    dump(sce2, "zeisel-brain-sparse-results-delayed-external", DelayedArray.preserve.ops=TRUE, DelayedArray.force.external=TRUE)
+
+    res <- analyze(as.matrix(counts(sce)), adt.x=as.matrix(counts(altExp(sce))), rna.subsets=list(Mito=grepl("^mt-", rownames(sce))))
+    sce2 <- convertAnalyzeResults(res)
+    dump(sce2, "zeisel-brain-dense-multimodal-results")
+    dump(sce2, "zeisel-brain-dense-multimodal-results-delayed", DelayedArray.preserve.ops=TRUE)
+    dump(sce2, "zeisel-brain-dense-multimodal-results-delayed-external", DelayedArray.preserve.ops=TRUE, DelayedArray.force.external=TRUE)
+})
